13700bd686cb7baee6ffb3304cf3df476f43e7a8
wendy <wendy@renzhoulongs-iMac.local>
2021-11-30 18:03:20 +0800

    添加校验

--- src/view/user/login.vue
+++ src/view/user/login.vue
@@ -4,7 +4,8 @@
     <div class="login_box">
       <div class="title">咖咖账号{{btn_text}}</div>
       <div class="ipt_box">
-        <input type="text" name="" placeholder="邮箱/手机号" v-model="username" id="">
+        <input type="text" name="" placeholder="邮箱/手机号" v-model="username" @blur="checkPhoneOrEmail()" id="">
+        <div class="err_text" v-if="emailIsError">{{email_error_tip}}</div>
       </div>
       <div class="ipt_box">
         <input type="password" name="" placeholder="密码" v-model="password" id="">
@@ -16,7 +17,7 @@
       <div class="agree_box">
         <van-checkbox v-model="checked" shape="square" icon-size="12" class="agree_text">已阅读并同意《咖咖隐私政策》《咖咖用户协议》</van-checkbox>
       </div>
-      <van-button type="info" block class="login_btn">{{btn_text}}</van-button>
+      <van-button type="info" block class="login_btn" @click="onClickButton()">{{btn_text}}</van-button>
       <div class="text_box">
         <div @click="forgetPsw()">忘记密码？</div>
         <div v-if="!ifShowRePsw" @click="toRegister()">手机注册</div>
@@ -39,24 +40,80 @@
 </template>
 
 <script>
+import { Toast } from "vant"
 export default {
   data() {
     return {
       username: '',
       password: '',
       re_password: '',
-      checked: "", //checked agreement
+      checked: false, //checked agreement
       ifShowRePsw: false,
       btn_text: "登录", //按钮名
       isError: false,
+      emailIsError: false,  //邮箱格式是否有误
+      email_error_tip: "邮箱格式有误",
     }
   },
   methods: {
+    // 点击登录/注册
+    onClickButton() {
+      console.log("checkde",this.checked)
+      if(this.ifShowRePsw) {
+      // 注册
+        if(!this.username) {
+          Toast("邮箱或手机号不能为空")
+        }else if(!this.password) {
+          Toast("密码不能为空")
+        }else if(!this.password) {
+          Toast("重复输入的密码不能为空")
+        }else if(this.checked == false) {
+          Toast("勾选同意协议后才能注册")
+        }
+      }else {  
+      // 登录
+        if(this.checked == false) {
+          Toast("勾选同意协议后才能登录")
+        }
+      }
+    },
+    // 校验邮箱/手机号
+    checkPhoneOrEmail() {
+      let email_test = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
+      let phone_test = /^1[0-9]{10}$/
+      let flag = false
+      console.log("邮箱校验",email_test.test(this.username))
+      if( this.username != '') {
+        if(!phone_test.test(this.username) && !flag){
+          this.email_error_tip = "请输入正确的手机号"
+          flag = false
+        }else {
+          this.email_error_tip = ""
+          flag = true
+        }
+        if(!flag) {
+          if(!email_test.test(this.username)) {
+            this.email_error_tip = "请输入正确的邮箱"
+            flag = false
+          }else {
+            this.email_error_tip = ""
+            flag = true
+          }
+        }
+        this.emailIsError = true
+      }else {
+        this.email_error_tip = ""
+        this.emailIsError = false
+      }
+      
+    },
     // 检验两次输入的密码是否相同
     checkPsw() {
       console.log("校验密码一致",this.password,this.re_password)
       if((this.password != this.re_password) && this.ifShowRePsw) {
         this.isError = true
+      }else {
+        this.isError = false
       }
     },
     // 用户登录
