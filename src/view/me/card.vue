13c2380ecdcce71db73ae71165aaeef29836a3cb
wendy <wendy@renzhoulongs-iMac.local>
2022-01-04 17:37:36 +0800

    text_fold效果

--- src/view/me/card.vue
+++ src/view/me/card.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    card
+  <div class="card_box">
+    <div class="fold_text">Astro Boy</div>
   </div>
 </template>
 
@@ -10,6 +10,50 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.card_box{
+  // margin: 0;
+  // padding: 0;
+  .fold_text{
+      text-transform: uppercase;/* 转成大写 */
+      width: 1000px;
+      margin: 200px auto;
+      font-size: 100px;
+      color: #fd79a8;
+      letter-spacing: 5px;/* 设置字间距 */
+      transition: all 1s;
+      -webkit-text-stroke: 2px #fd79a8;/* 设置字体边框 ie浏览器不支持*/
+      transform: rotate(-5deg);/* 旋转一定角度 */
+  }
+  .fold_text::before {
+      content: 'Astro Boy';/* 阿童木 */
+      position: absolute;
+      top: 0;
+      left: 0;
+      color: white;
+      clip-path: inset(0 0 50% 0);
+      transform: rotate(0deg) skew(0deg);
+      transition: all 1s;
+  }
+  .fold_text:hover::before {
+      transform: rotateX(-30deg) skew(-30deg);
+      color: (243,243,243);
+      text-shadow: 0 0 2px black;
+  }
+  .fold_text::after {
+      content: 'Astro Boy';
+      position: absolute;
+      top: 0;
+      left: 0;
+      color: white;
+      clip-path: inset(50% 0 0 0);/* 裁剪半个矩形区域 */
+      transform: rotate(0deg) skew(0deg);
+      transition: all 1s;
+  }
+  .fold_text:hover::after {
+      transform: rotateX(40deg) skew(20deg);
+      color: (243,243,243);
+      text-shadow: 0 0 2px black;
+  }
+}
 </style>
--- src/view/me/store.vue
+++ src/view/me/store.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    store
+  <div class="store_page">
+    <!--  绘制圆 -->
+    <div class="face">
+      <div class="eyes">
+        <div class="eye">
+          <div class="eyeball">
+          </div>
+        </div>
+        <div class="eye">
+          <div class="eyeball">
+          </div>
+        </div>
+      </div>
+      
+    </div>
   </div>
 </template>
 
@@ -11,5 +24,42 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.store_page{
+  .face{
+    width: 100px;
+    height: 100px;
+    background: linear-gradient(rgb(132, 222, 247), rgb(14, 84, 175));
+    // left: 50%;
+    // top: 50%;
+    // transform: translate(-50%,-50%);
+    position: absolute;
+    border-radius: 50%;
+    .eyes {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: space-evenly;
+      padding-top: 25%;
+      .eye{
+        width: 13%;
+        height: 25%;
+        background: linear-gradient(135deg, rgb(82, 83, 83), rgb(14, 15, 15));
+        border-radius: 50%;
+        .eyeball::before {
+          content: '';
+          position: absolute;
+          top: 50%;
+          left: 15px;
+          transform: translate(-50%, -50%);
+          width: 20px;
+          height: 30px;
+          background-color: white;
+          border-radius: 50%;
+          box-shadow: 0px 0px 10px white;
+        }
+      }
 
+    }
+  }
+}
 </style>
